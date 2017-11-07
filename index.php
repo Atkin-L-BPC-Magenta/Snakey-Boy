c1a2ed64778b1c4f9b1f623ac771d85a677bdd91
Louis Atkin <32098761+Atkin-L-BPC-Magenta@users.noreply.github.com>
2017-11-07 13:53:33 +0000

    Update index.php

--- index.php
+++ index.php
@@ -20,32 +20,32 @@
        }
  
   
-  switch($dice)
-  {
-   case 1:
-    echo "you rolled a 1";
-    break;
+    switch($dice)
+    {
+    case 1:
+     echo "you rolled a 1";
+     break;
     
     
     case 2:    
-    echo "you rolled a 2";    
-    break;
+     echo "you rolled a 2";    
+     break;
     
     case 3:
-    echo "you rolled a 3";
-    break;
+     echo "you rolled a 3";
+     break;
     
     case 4:    
-    echo "you rolled a 4";
-    break;
+     echo "you rolled a 4";
+     break;
     
     case 5:    
-    echo "you rolled a 5";
-    break;
+     echo "you rolled a 5";
+     break;
     
     case 6:
-    echo "you rolled a 6";
-    break;
+     echo "you rolled a 6";
+     break;
   }
  ?>
  </body> 
